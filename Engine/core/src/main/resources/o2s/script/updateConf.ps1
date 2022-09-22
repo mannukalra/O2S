38f9f63c8e3f6fb6c2e1a8e2e42cf696d95fc447
mannukalra <mkmandeepkalra@gmail.com>
2022-09-22 16:26:38 +0530

    ps1 to luanch agent seperate process

--- Engine/core/src/main/resources/o2s/script/updateConf.ps1
+++ Engine/core/src/main/resources/o2s/script/updateConf.ps1
@@ -13,5 +13,9 @@ add-type @"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -UseBasicParsing http://192.168.1.36:8090/config -OutFile telegraf/telegraf.conf
 
-cd telegraf
-.\telegraf.exe --config telegraf.conf
+#get id from below cmd and kill 
+Get-Process telegraf 
+#wait 5 sec 
+
+Start-Process -WorkingDirectory "C:\Users\Administrator\WIN-5AQBA3PBPPH.mshome.net\o2s\telegraf" -FilePath .\telegraf.exe -ArgumentList "--config", "telegraf.conf", "> telegraf.log"
+
