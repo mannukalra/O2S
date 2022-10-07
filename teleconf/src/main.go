a3ce68619fea0bad2bb8de5cddb084a1bffcd7d2
mannukalra <mkmandeepkalra@gmail.com>
2022-10-07 17:15:21 +0530

    Grafana client testing

--- teleconf/src/main.go
+++ teleconf/src/main.go
@@ -1,14 +1,62 @@
 package main
 
 import (
+	// "fmt"
+	//server "teleconf/handlers"
 	"fmt"
-	server "teleconf/handlers"
+	"net/url"
 	tgplugins "teleconf/util"
+
+	gapi "github.com/grafana/grafana-api-golang-client"
 )
 
 func main() {
 
-	fmt.Println(tgplugins.InputPlugins())
+	// fmt.Println(tgplugins.InputPlugins())
+	tgplugins.InputPlugins()
+	// server.StartServer()
+	client, err := gapi.New("http://localhost:3003/", gapi.Config{BasicAuth: url.UserPassword("admin", "admin")})
+	if err != nil {
+		fmt.Printf("expected error to be nil; got: %s", err.Error())
+	}
 
-	server.StartServer()
+	dashboard, err := client.DashboardByUID("mannuvm")
+	if err != nil {
+		fmt.Printf("expected error to be nil; got: %s", err.Error())
+	}
+
+	fmt.Println(">>>>>>>>>>>>>>>>MANNUVMDASHBOARD>>>>>>>>>>>" + dashboard.Meta.URL)
+
+	// dashb := gapi.Dashboard{
+	// 	Model: map[string]interface{}{
+	// 		"title": "testDashboard",
+	// 	},
+	// 	FolderID:  dashboard.Meta.Folder,
+	// 	FolderUID: dashboard.FolderUID,
+	// 	Overwrite: false,
+	// }
+	// resp, err := client.NewDashboard(dashb)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// fmt.Println(resp)
+
+	// if resp.UID != "nErXDvCkzz" {
+	// 	fmt.Println("Invalid uid - %s, Expected %s", resp.UID, "nErXDvCkzz")
+	// }
+
+	panel := gapi.LibraryPanel{
+		Folder: dashboard.Meta.Folder,
+		Name:   "API docs Example",
+		Model:  map[string]interface{}{"description": "", "type": ""},
+	}
+
+	resp1, err := client.NewLibraryPanel(panel)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(resp1)
+
 }
